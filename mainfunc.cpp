ad61b31753d05eb2ab8b230694a7ec88cf2e0e06
Saleh Taerian <saleh1321385@gmail.com>
2025-03-27 12:51:32 +0330

    writeininfofile kamel dorost shod

--- mainfunc.cpp
+++ mainfunc.cpp
@@ -55,46 +55,47 @@ void signin()
 }
 void writeininfofile(karbar user1)
 {
-    FILE *fp;
-    fp = fopen("info.json", "a+");
-    if (fp == nullptr)
-    {
-        cout << "error in openning file" << endl;
-        exit(0);
-    }
+    FILE *fp = fopen("info.json", "r");
     json jsonarray;
-    fseek(fp, 0, SEEK_END);
-    int fpsize = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-    if (fpsize == 0)
+    if (fp != nullptr)
+    {
+        fseek(fp, 0, SEEK_END);
+        int fpsize = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+        if (fpsize > 0)
+        {
+            char *readfile = new char[fpsize + 1];
+            fread(readfile, 1, fpsize, fp);
+            readfile[fpsize] = '\0';
+            fclose(fp);
+            try
+            {
+                jsonarray = json::parse(readfile);
+            }
+            catch (const json::parse_error)
+            {
+                cout << "dobareh parse" << endl;
+                delete[] readfile;
+                return;
+            }
+            delete[] readfile;
+        }
+        else
+        {
+            jsonarray = json::array();
+            fclose(fp);
+        }
+    }
+    else
     {
         jsonarray = json::array();
-        jsonarray.push_back({{"username", user1.usernamegetter()}, {"password", user1.passwordgetter()}, {"name", user1.namegetter()}, {"lastname", user1.lastnamegetter()}});
-        int size = jsonarray.size();
-        string info = jsonarray.dump(size);
-        int infosize = info.size();
-        char *infoarray = new char[infosize];
-        strcpy(infoarray, info.c_str());
-        fprintf(fp, "%s", infoarray);
-        delete[] infoarray;
     }
-    if (fpsize > 0)
-    {
-        char *readfile = new char[fpsize+1];
-        fread(readfile,1 , fpsize, fp);
-        readfile[fpsize] = '\0';
-        jsonarray = json::parse(readfile);
-        jsonarray.push_back({{"username", user1.usernamegetter()}, {"password", user1.passwordgetter()}, {"name", user1.namegetter()}, {"lastname", user1.lastnamegetter()}});
-        delete[] readfile;
-        int size = jsonarray.size();
-        string info = jsonarray.dump(size);
-        int infosize = info.size();
-        char *infoarray = new char[infosize];
-        strcpy(infoarray, info.c_str());
-        fclose(fp);
-        fp = fopen("info.json", "w+");
-        fprintf(fp, "%s", infoarray);
-        delete[] infoarray;
-    }
+    jsonarray.push_back({{"name", user1.namegetter()},
+                         {"lastname", user1.lastnamegetter()},
+                         {"username", user1.usernamegetter()},
+                         {"password", user1.passwordgetter()}});
+    fp = fopen("info.json", "w");
+    string info = jsonarray.dump(4);
+    fprintf(fp, "%s", info.c_str());
     fclose(fp);
 }
